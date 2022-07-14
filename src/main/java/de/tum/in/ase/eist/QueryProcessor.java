05518312083826e525aacefe799279c22054d616
konrad2002 <konrad@schwimmteamerzgebirge.de>
2022-07-14 17:13:48 +0200

    plus

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,6 +12,16 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
+            return "MyTeam";
+        } else if (query.contains("what is")) {
+            if (query.contains("plus")) {
+                String s2 = query.substring(query.indexOf("what is "), query.indexOf("plus"));
+                String s3 = query.substring(query.indexOf("plus"));
+                int i1 = Integer.parseInt(s2);
+                int i2 = Integer.parseInt(s3);
+                int o3 = i1 + i2;
+                return String.valueOf(o3);
+            }
            return "MyTeam";
         } else { // TODO extend the programm here
             return "";
