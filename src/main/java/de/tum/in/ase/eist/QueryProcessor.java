db31d058561e0265ff26db14f4d850097bac9699
konrad2002 <konrad@schwimmteamerzgebirge.de>
2022-07-14 17:18:28 +0200

    add

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,15 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
             return "MyTeam";
-        } else if (query.contains("what is")) {
-            if (query.contains("plus")) {
+        } else if (query.contains("plus")) {
+                query = query.replaceFirst("what is ", "");
+                String s2 = query.substring(0, query.indexOf("plus"));
+                String s3 = query.substring(query.indexOf("plus"));
+                int i1 = Integer.parseInt(s2);
+                int i2 = Integer.parseInt(s3);
+                int o3 = i1 + i2;
+                return String.valueOf(o3);
+        } else if (query.contains("largest")) {
                 String s2 = query.substring(query.indexOf("what is "), query.indexOf("plus"));
                 String s3 = query.substring(query.indexOf("plus"));
                 int i1 = Integer.parseInt(s2);
@@ -22,8 +29,7 @@ public class QueryProcessor {
                 int o3 = i1 + i2;
                 return String.valueOf(o3);
             }
-           return "MyTeam";
-        } else { // TODO extend the programm here
+        else { // TODO extend the programm here
             return "";
         }
     }
